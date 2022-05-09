da2e16d4b6b916ce3b733aeb8c1e7d50e571e391
chayut005 <64309010005@chontech.ac.th>
2022-05-09 08:45:21 +0700

    project

---
+++ application/models/Connect_db.php
@@ -0,0 +1,98 @@
+<?php
+class Connect_db extends CI_Model {
+
+	public function __construct()
+	{
+		// $this->cmm = $this->load->database('qmdm_system', true);
+	}
+
+	public function test()
+	{
+		redirect('manage');
+				
+    }
+	
+
+
+	 public function Login($usr='',$pwd='')
+    {
+		$p['usr'] = trim($usr);
+		$p['pwd'] = base64_encode(trim($pwd));
+		
+		$sqlSel = "SELECT * FROM sys_users WHERE username='{$p['usr']}' and password='{$p['pwd']}';";
+		$query = $this->db->query($sqlSel);
+				
+		if($query->num_rows()!=0) {
+			
+			$result = $query->result_array();
+						
+			if($result['0']['enable']!=0){
+					
+				return $result[0];
+				
+			}else{
+				
+				$error = array('action'=>'err', 'value'=>'b');			
+				return $error;
+			}
+			
+		}else { 
+			
+			$error = array('action'=>'err', 'value'=>'i');			
+			return $error;
+		}
+    }
+	public function Logout()
+	{	
+		/*$sqlDelFile = "DELETE FROM upload_temp_filename WHERE u_id='".$this->session->userdata('sessUsrId')."';";
+		$excDelFile = $this->db->query($sqlDelFile);*/
+		
+		$sqlLastAcc = "UPDATE sys_users SET last_access=NOW() WHERE username='".$this->session->userdata('sessUsr')."';";
+            $this->session->unset_userdata($sqlLastAcc);
+            $this->session->sess_destroy();
+
+            redirect('login/usera');
+		
+	}
+
+	public function CheckPermission($para){
+		
+		$get_url = trim($this->router->fetch_class().'/'.$this->router->fetch_method());
+
+		$sqlChkPerm = "SELECT
+			sp.name,
+			sp.controller
+			FROM
+			sys_users_permissions AS sup
+			INNER JOIN sys_permissions AS sp ON sp.sp_id = sup.sp_id
+			LEFT JOIN sys_permission_groups AS spg ON sp.spg_id = spg.spg_id
+			WHERE
+			sp.enable='1' AND spg.enable='1' AND sup.su_id='{$para}' AND sp.controller='{$get_url}';";
+			
+		$excChkPerm = $this->db->query($sqlChkPerm);
+		$numChkPerm = $excChkPerm->num_rows();
+		
+		if($numChkPerm == 0) {
+			
+			echo '<script language="javascript">';
+			echo 'alert("Permission not found.");';
+			echo 'history.go(-1);';
+			echo '</script>';
+			exit();
+			
+		}
+
+	}
+    public function CheckSession() 
+    {
+        if($this->session->userdata('loggedIn')!="OK") {
+					
+           redirect('login/account');
+		   return FALSE;
+		   
+        }else{	return TRUE; 	}
+    }
+
+}
+ 
+?>
---
+++ application/views/all/page/view_menu.php
@@ -0,0 +1,75 @@
+<style>
+  .chid{
+    margin-left:-50px;
+  }
+  .zzz{
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+  }
+</style>
+<header class="side-header">
+
+<a href="#">
+  <div class="logo">
+    <img class="zzz" src="https://s.isanook.com/ns/0/rp/r/w850/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL25zLzAvdWQvMzE5LzE1OTY2MzMvdG9vLmpwZw==.jpg">
+  </div>
+</a>
+
+<nav class="navbar-side">
+  
+  <ul>
+  <!--class="active"-->
+    <li><a style="font-size:20px;" href="">
+      <div class="svg-space chid"></div><i class="fas fa-user-circle bigger-180">&nbsp</i><?php echo ": ".$this->session->userdata('sessName');?></a>
+    </li>
+    <hr>
+    <li><a href="labs.html">
+      <div class="svg-space chid"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 64 80" enable-background="new 0 0 64 64" xml:space="preserve"></svg></div><i class="fa fa-paper-plane bigger-180">&nbsp&nbsp</i> Request</a>
+    </li>
+    <li><a href="labs.html">
+      <div class="svg-space chid"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 64 80" enable-background="new 0 0 64 64" xml:space="preserve"></svg></div><i class="fa fa-table bigger-180">&nbsp&nbsp</i> Table Account</a>
+    </li>
+    <li><a href="contact.html">
+      <div class="svg-space chid"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 64 80" enable-background="new 0 0 64 64" xml:space="preserve"></svg></div><i class="fa fa-user-plus bigger-180">&nbsp&nbsp</i> Register</a>
+    </li>
+
+    <li class="bottom"><a href="<?php echo site_url('manage/logout'); ?>">
+      <div class="svg-space"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 64 80" enable-background="new 0 0 64 64" xml:space="preserve"></svg></div><i class="ace-icon fa fa-power-off bigger-180">&nbsp</i> Logout </a>
+    </li>
+
+  </ul>
+
+</nav>
+
+</header>
+
+<main class="main-wrapper">
+
+<header class="top-navbar">
+  
+  <div>
+  <img class="animate__animated animate__heartBeat" src="<?php echo base_url(); ?>./help/page/img/help.png"  height="80"/>
+  </div>
+
+
+<!--
+  <nav class="navbar-top">
+    
+    <ul>
+      <li>
+      <div class="nav-search" id="nav-search">
+							<form class="form-search">
+								<span class="input-icon">
+									<input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+									<i class="ace-icon fa fa-search nav-search-icon"></i>
+								</span>
+							</form>
+      </div>
+      </li>
+
+    </ul>
+
+  </nav> -->
+
+</header>
